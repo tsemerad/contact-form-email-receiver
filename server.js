e9c7cc88875d22fe2b4c5a16f49a6d07ae4d0956
Tanner Semerad <tanner.semerad@alseageo.net>
2016-02-14 21:02:43 -0800

    responding to contact form

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 #!/bin/env node
-//  OpenShift sample Node application
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
 var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
 var fs      = require('fs');
+var nodemailer = require('nodemailer');
 
 
 /**
- *  Define the sample application.
+ *  Define the application.
  */
 var SampleApp = function() {
 
@@ -134,11 +139,24 @@ var SampleApp = function() {
     self.initializeServer = function() {
         self.createRoutes();
         self.app = express();
+        self.app.use(bodyParser());
+
+        //CORS middleware
+        var allowCrossDomain = function(req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+            next();
+        }
+
+        self.app.use(allowCrossDomain);
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
             self.app.get(r, self.routes[r]);
         }
+        self.app.post('/', handleContactForm);
     };
 
 
@@ -169,6 +187,61 @@ var SampleApp = function() {
 };   /*  Sample Application.  */
 
 
+function handleContactForm(req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  var receivers = process.env.MAIN_RECIPIENT_EMAIL;
+
+  console.log('req is', req);
+  var source = req.body.contactsource;
+  if (source === 'badlandstaxsolutions.com') {
+    console.log('SHOULD add Debbi\'s email');
+    // receivers += ', ' + process.env.BADLANDS_EMAIL;
+  }
+
+  var contentHoneypot = req.body.content;
+
+  if (contentHoneypot) {
+    console.error('They filled out the honeypot, must be robots!');
+  }
+
+  var plainText = 'A visitor filled out your contact form:\n';
+  plainText += 'Name: ' + req.body.name;
+  plainText += 'Email: ' + req.body.email;
+  plainText += 'Message: ' + req.body.message;
+
+  var html = '<h3>A visitor filled out your contact form:</h3>';
+  html += '<ul>';
+  html += '<li><b>Name:</b> ' + req.body.name + '</li>';
+  html += '<li><b>Email:</b> ' + req.body.email + '</li>';
+  html += '<li><b>Message:</b> ' + req.body.message + '</li>';
+  html += '</ul>';
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: 'tsemcontact<tsemcontact@gmail.com>', // sender address
+    to: receivers, // list of receivers
+    subject: 'New contact message from ' + source,
+    text: plainText,
+    html: html
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+    return res.send('Message sent');
+    // return res.end('hello,world\nkeesun,hi', 'UTF-8');
+  });
+}
 
 /**
  *  main():  Main code.
@@ -176,4 +249,3 @@ var SampleApp = function() {
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
-
